59b7a3c9f226a69f27ad63caf3faa264ec39b2c5
Gaurav Sharma <igaurav1002@outlook.com>
2025-11-05 12:05:00 +0000

    add azcli file and removed code for RG VNET Subnet

--- modules/linux_vm_v1/main.tf
+++ modules/linux_vm_v1/main.tf
@@ -1,34 +1,6 @@
-resource "azurerm_resource_group" "default" {
-  count = var.resource_group_name == "" ? 1 : 0
-  name     = var.vm_name != "" ? "rg-${var.vm_name}" : "rg-default"
-  location = var.location
-}
-
-# Create VNet if subnet_id is not provided
-resource "azurerm_virtual_network" "default" {
-  count               = var.subnet_id == "" ? 1 : 0
-  name                = var.vnet_name
-  address_space       = var.address_space
-  location            = var.location
-  resource_group_name = var.resource_group_name != "" ? var.resource_group_name : azurerm_resource_group.default[0].name
-}
-
-# Create subnet if subnet_id is not provided
-resource "azurerm_subnet" "default" {
-  count                = var.subnet_id == "" ? 1 : 0
-  name                 = var.subnet_name
-  resource_group_name  = var.resource_group_name != "" ? var.resource_group_name : azurerm_resource_group.default[0].name
-  virtual_network_name = azurerm_virtual_network.default[0].name
-  address_prefixes     = [cidrsubnet(var.address_space[0], 8, 0)]
-}
-
-
 data "azurerm_resource_group" "myrg" {
-  count = var.resource_group_name != "" ? 1 : 0
-  name  = var.resource_group_name != "" ? var.resource_group_name : azurerm_resource_group.default[0].name
+  name  = var.resource_group_name
 }
-
-
 
 resource "azurerm_network_interface" "mynic" {
   name                = "${var.vm_name}-nic-${count.index + 1}"
